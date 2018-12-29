2244e27d97d2c7415bf1903baced88a233a2b023
JAUME PARROT ALTISENT <0015748@insags.com>
2018-12-30 00:44:40 +0100

    Instalar vue-router y configurar router view

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,17 @@
 <template lang="pug">
   #app
     fm-header
-
-    fm-notification(v-show="showNotification")
-      p(slot="body") No se encontraron resultados
-
-    fm-loader(v-show="isLoading")
-    section.section(v-show="!isLoading")
-      nav.nav
-        .container
-          .field.has-addons
-            input.input.is-large(
-              type="text",
-              placeholder="Busacar canciones",
-              v-model="searchQuery"
-            )
-            a.button.is-info.is-large(@click="search") Buscar
-            a.button.is-danger.is-large &times;
-      .container
-        p.help.is-info.has-text-righ
-          small {{searchMessage}}
-      .container.results
-        .columns.is-multiline
-          .column.is-one-quarter(v-for="t in tracks")
-            fm-track(
-              :class="{ 'is-active': t.id === selectedTrack }",
-              :track="t",
-              @select="setSelectedTrack"
-            )
+    router-view
     fm-footer
 </template>
 
 <script>
-import trackService from '@/services/track'
-
 import FmFooter from '@/components/layout/Footer.vue'
 import FmHeader from '@/components/layout/Header.vue'
 
-import FmTrack from '@/components/Track.vue'
-
-import FmNotification from '@/components/shared/Notification.vue'
-import FmLoader from '@/components/shared/Loader.vue'
-
 export default {
   name: 'app',
-  components: { FmFooter, FmHeader, FmTrack, FmLoader, FmNotification },
-  data () {
-    return {
-      searchQuery: '',
-      tracks: [],
-      isLoading: false,
-      showNotification: false,
-      selectedTrack: ''
-    }
-  },
-  computed: {
-    searchMessage () {
-      return `Encontrados: ${this.tracks.length}`
-    }
-  },
-  watch: {
-    showNotification () {
-      if (this.showNotification) {
-        setTimeout(() => {
-          this.showNotification = false
-        }, 3000)
-      }
-    }
-  },
-  methods: {
-    search () {
-      if (!this.searchQuery) {
-        this.tracks = []
-        return
-      }
-
-      this.isLoading = true
-
-      trackService.search(this.searchQuery)
-        .then(res => {
-          this.showNotification = res.tracks.total === 0
-          this.tracks = res.tracks.items
-          this.isLoading = false
-        })
-    },
-    setSelectedTrack (id) {
-      this.selectedTrack = id
-    }
-  }
+  components: { FmFooter, FmHeader }
 }
 </script>
 
